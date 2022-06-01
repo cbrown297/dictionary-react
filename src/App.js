4842339c85e1a31495f29a1619c72f0504a6c166
Claudia Brown <claudiabrown297@gmail.com>
2022-06-01 00:19:50 -0400

    Taking off the underline from links and fixed export default on App component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import DictionarySearch from './DictionarySearch';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <h1>Dictionary React App</h1>
@@ -16,5 +16,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
