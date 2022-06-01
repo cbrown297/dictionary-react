445862d5b2ffc68d04b49d48805ee98e74516474
Claudia Brown <claudiabrown297@gmail.com>
2022-06-01 18:21:06 -0400

    creating a results and meaning component with api integrated

---
+++ src/Meanings.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Meanings(props) {
+  console.log(props.meaning);
+  return ( 
+    <div className="Meanings">
+      <h3>{props.meaning.partOfSpeech}</h3>
+      <p>{props.meaning.defintions[0].definition}</p>
+      <p>{props.meaning.defintions[0].example}</p>
+    </div>
+  );
+}
