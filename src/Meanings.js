9842300bd755545d3a25aafe59ea8a2a7af0ee97
Claudia Brown <claudiabrown297@gmail.com>
2022-06-04 23:45:40 -0400

    trying to add synonyms to page

--- src/Meanings.js
+++ src/Meanings.js
@@ -1,17 +1,20 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meanings(props) {
-  console.log(props.meaning);
   return ( 
     <div className="Meanings">
       <h3>{props.meaning.partOfSpeech}</h3>
       {props.meaning.definitions.map(function(definitions, index) {
         return (
           <div key={index}>
-            <p>{definitions.definition}
-            <br />
-            <em>{definitions.example}</em>
+            <p>
+              <strong>Definition:</strong>{definitions.definition}
+              <br />
+              <strong>Example:</strong>
+              <em>{definitions.example}</em>
             </p>
+            <Synonyms synonyms={definitions.synonyms} />
           </div>
         );
       })}
